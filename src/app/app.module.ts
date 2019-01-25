a503a1568b3f9d34885d3098d4e078aeb210a27b
Maciek <maciej.plonski@pagepro.pl>
2019-01-25 19:51:02 +0100

    exercise 1,2,3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgxEditorModule } from "ngx-editor";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { TestItemComponent } from './test-item/test-item.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { MailContentComponent } from './mail-content/mail-content.component';
-import { InboxComponent } from './inbox/inbox.component';
-import { DraftsComponent } from './drafts/drafts.component';
-import { SentComponent } from './sent/sent.component';
-import { AllMailComponent } from './all-mail/all-mail.component';
-import { EmailService } from './email';
+import { AppComponent } from "./app.component";
+import { TestItemComponent } from "./test-item/test-item.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { MailContentComponent } from "./mail-content/mail-content.component";
+import { InboxComponent } from "./inbox/inbox.component";
+import { DraftsComponent } from "./drafts/drafts.component";
+import { SentComponent } from "./sent/sent.component";
+import { AllMailComponent } from "./all-mail/all-mail.component";
+import { EmailService } from "./email";
 
 const routes: Routes = [];
 
@@ -31,12 +33,12 @@ const routes: Routes = [];
     BrowserModule,
     FormsModule,
     RouterModule.forRoot(routes),
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    NgxEditorModule,
+    HttpClientModule
   ],
   exports: [RouterModule],
-  providers: [
-    EmailService
-  ],
+  providers: [EmailService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
