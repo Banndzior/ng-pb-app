52488c7e498a06c2ac01ce57135077d62c985361
Maciek <maciej.moroz89@gmail.com>
2019-02-19 22:58:53 +0100

    id elementu

--- src/app/inbox/inbox.component.ts
+++ src/app/inbox/inbox.component.ts
@@ -1,26 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { InboxEmailMessage, EmailService } from '../email';
+import { Component, OnInit, Injectable } from "@angular/core";
+import { InboxEmailMessage, EmailService } from "../email";
+import { NgbPanelChangeEvent } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-inbox',
-  templateUrl: './inbox.component.html',
-  styleUrls: ['./inbox.component.scss']
+  selector: "app-inbox",
+  templateUrl: "./inbox.component.html",
+  styleUrls: ["./inbox.component.scss"]
 })
 export class InboxComponent implements OnInit {
   inboxMessages: InboxEmailMessage[] = [];
+  selectedId = "static-0";
 
-  constructor(
-    public emailService: EmailService
-  ) { }
-
+  constructor(public emailService: EmailService) {}
   ngOnInit() {
-    console.log('InboxComponent.ngOnInit()');
+    this.selectedId = this.selectedId;
+    console.log("InboxComponent.ngOnInit()");
 
-    this.emailService.emailSentEvent.subscribe((title) => {
-      console.log('emailService.emailSentEvent', title);
+    this.emailService.emailSentEvent.subscribe(title => {
+      console.log("emailService.emailSentEvent", title);
     });
 
-    this.emailService.getInboxMessages()
-      .then((result) => this.inboxMessages = result);
+    this.emailService
+      .getInboxMessages()
+      .then(result => (this.inboxMessages = result));
+  }
+  public toggleAccordian($event: NgbPanelChangeEvent) {
+    this.selectedId = $event.panelId;
+    console.log(this.selectedId);
   }
 }
